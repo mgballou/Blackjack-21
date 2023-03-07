875ba326e8292a3723dbb2d837fbf980d940c3ba
mgballou <matthewbuiltthat@gmail.com>
2023-03-07 14:52:43 -0500

    Empty code blocks for necessary classes. Set testing values in my init function to render to page. renderHands function adds card elements to the page as expected

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 let deck // will hold the new instance of the deck class when init is run
 let dCards // will hold an array of the dealer's cards
 let pCards // will hold an array of the player's cards
+let money // stores the player's current winnings
 
 //------ DOM elements
 const dealerHandEl = document.querySelector('#dealer-hand') // display area to append card elements to
@@ -12,16 +13,54 @@ const messagesEl = document.querySelector('#messages') // location for messages 
 
 //------ classes
 
+class Deck {
+
+}
+
+class PlayingCard {
+
+}
+
+class Dealer {
+
+}
+
 
 //------ functions
 function init() {
+    
     console.log('Game start')
+    dCards = [1,2,3]
+    pCards = ['Ace',7,2]
     render()
 }
 
 function render() {
     console.log('Rendering game')
+    renderHands()
+
 }
+function renderHands() {
+    //// iterates over dCards array, creating a new DOM element and appending it to the parent div
+    dCards.forEach(card => {
+        // will need to add an if-statement here to check if an element already exists and to skip that index of the array if so on subsequent calls of render
+        let newCardEl = document.createElement('div')
+        newCardEl.classList.add('card')
+        newCardEl.textContent = card // will need to refactor this to accept all the information that will actually be contained in each card in the array (suit, value)
+        dealerHandEl.appendChild(newCardEl)
+    })
+
+    pCards.forEach(card => {
+        // will need to add an if-statement here to check if an element already exists and to skip that index of the array if so on subsequent calls of render
+        let newCardEl = document.createElement('div')
+        newCardEl.classList.add('card')
+        newCardEl.textContent = card // will need to refactor this to accept all the information that will actually be contained in each card in the array (suit, value)
+        playerHandEl.appendChild(newCardEl)
+    })
+
+}
+
+
 
 function hit() {
 
