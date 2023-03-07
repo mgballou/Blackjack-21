103b3a7681c4a874d4f578b37a9f099def66baf3
mgballou <matthewbuiltthat@gmail.com>
2023-03-07 09:05:14 -0500

    first commit

---
+++ psuedocode.js
@@ -0,0 +1,23 @@
+//------- app state
+//// Set the inital state of necessary variables, these things all will most likely need to be rendered to the screen
+//// deck, playerHand, dealerHand
+//------ DOM elements
+//// Cache DOM elements for use in my render functions
+//// gameBoardEl, hiddenCardEl, showingCardEl, messagesEl, buttonEls
+
+//------ classes
+//// necessary classes for major bits of game data
+//// Deck, Cards
+//// Methods for shuffling cards and dealing hands
+
+//------ functions
+//// I will define my callback functions that will be utilized by my event listeners & manage the flow of my game
+//// function hit => deals the player an additional card
+//// function stand => starts the dealer's turn
+//// function checkWinner => evaluates values of player and dealer's hands
+
+//------ Game
+//// Invoke the iniatialize() function, starting the game
+//// iniatialize creates instances of Deck & cards, shuffles deck, deals first hand
+//// Waits for the player to select 'hit' or 'stand'
+
